4ab4f858c63ff75b5f21401ef12c64c532bcd857
manideepreddypadamati <manideepreddypadamati@gmail.com>
2025-12-18 20:03:24 +0530

    added

---
+++ basics/variables.js
@@ -0,0 +1,18 @@
+// Js is scripting language for client-side 
+
+// Js is dynamically typed language
+// Variable - container to store values 
+// Variables are defined inside scope
+// let and const - block scope
+// var - function scope 
+// let - reassigned but not redeclared
+// const - not reassigned
+
+let fname = 'Manideep';
+const PI = 3.14;
+var a = 100;
+var a = 'sunny';
+
+console.log(fname);
+console.log(PI);
+console.log(a);
